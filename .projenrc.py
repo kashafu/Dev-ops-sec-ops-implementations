019cc84e860d6835f2914c7d49a5abbb34090753
Nicolas Byl <nicolas.byl@nexineer.io>
2023-03-20 23:22:55 +0100

    chore: setup project structure

--- .projenrc.py
+++ .projenrc.py
@@ -1,11 +1,19 @@
 from projen.python import PythonProject
 
 project = PythonProject(
-    author_email="nicolas.byl@nexineer.io",
+    author_email="nico@nicolas-byl.io",
     author_name="Nicolas Byl",
     module_name="taskman",
     name="taskman",
     version="0.1.0",
+    deps=[
+        'fastapi',
+        'uvicorn[standard]'
+    ]
 )
+
+dev_task = project.add_task('dev')
+dev_task.exec('uvicorn taskman.main:app --reload')
+
 
 project.synth()
