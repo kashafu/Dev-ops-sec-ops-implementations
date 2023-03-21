f046050c20555b5e0c242a66ed81f34b36182776
Nicolas Byl <nicolas.byl@nexineer.io>
2023-03-21 08:10:11 +0100

    feat: add rest api for todo items

--- taskman/main.py
+++ taskman/main.py
@@ -1,15 +1,48 @@
-from typing import Union
+# -*- coding: utf-8 -*-
+from typing import Dict
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class TodoItemRequest(BaseModel):
+    name: str
+    description: str
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+class TodoItem(TodoItemRequest):
+    item_id: int
+
+
+items: Dict[str, TodoItem] = {}
+
+
+@app.get('/items')
+def get_items() -> list[TodoItem]:
+    return list(items.values())
+
+
+@app.get('/items/{item_id}')
+def get_items(item_id: str) -> TodoItem:
+    return items[item_id]
+
+
+@app.put('/items/{item_id}')
+def update_item(item_id: str, item: TodoItemRequest) -> None:
+    items[item_id] = TodoItem(
+        item_id=item_id,
+        name=item.name,
+        description=item.description,
+    )
+
+
+@app.post('/items')
+def create_item(item: TodoItemRequest):
+    item_id = str(len(items) + 1)
+    items[item_id] = TodoItem(
+        item_id=item_id,
+        name=item.name,
+        description=item.description,
+    )
