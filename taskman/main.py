c53401b7fbd7f30d3526c9d779200f0ec3db29b8
Nicolas Byl <nico@nicolas-byl.eu>
2023-04-29 11:56:32 +0000

    start working on redis backend

--- taskman/main.py
+++ taskman/main.py
@@ -3,8 +3,12 @@ from typing import Dict, List
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+import json
+import redis
 
 app = FastAPI()
+
+r = redis.Redis(host='redis', port=6379, decode_responses=True)
 
 
 class TaskRequest(BaseModel):
@@ -21,21 +25,36 @@ tasks: Dict[str, Task] = {}
 
 @app.get('/tasks')
 def get_tasks() -> List[Task]:
-    return list(tasks.values())
+    keys = r.keys()
+
+    tasks = []
+    for key in keys:
+        print(key)
+        value = json.loads(r.get(key))
+        tasks.append(Task(
+            item_id=key,
+            name= value['name'],
+            description = value['description'],
+        ))
+    return tasks
 
 
 @app.get('/tasks/{item_id}')
 def get_task(item_id: str) -> Task:
-    return tasks[item_id]
+    value = json.loads(r.get(f'tasks:{item_id}'))
+    return Task(
+        item_id= item_id,
+        name= value['name'],
+        description = value['description'],
+    )
 
 
 @app.put('/tasks/{item_id}')
 def update_task(item_id: str, item: TaskRequest) -> None:
-    tasks[item_id] = Task(
-        item_id=item_id,
-        name=item.name,
-        description=item.description,
-    )
+    r.set(f'tasks:{item_id}', json.dumps({
+        'name': item.name,
+        'description': item.description,
+    }))
 
 
 @app.post('/tasks')
